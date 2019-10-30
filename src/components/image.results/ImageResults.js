e03ae3a9808bd508000fb0946709fd3cbc359520
joshseven <neves.joshua@gmail.com>
2019-10-30 10:14:07 -0400

    search bar function added

---
+++ src/components/image.results/ImageResults.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PropTypes from "prop-Types";
+
+class ImageResults extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+ImageResults.PropTypes = {
+  images: PropTypes.array.isRequired
+};
+export default ImageResults;
